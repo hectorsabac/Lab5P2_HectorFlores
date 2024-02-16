ec32d55660926ca0b2bbbaf694f5e72116d2cd5d
PAOmontoya <123204626+PAOmontoya@users.noreply.github.com>
2024-02-16 13:45:16 -0600

    Add files via upload

---
+++ Facebook.java
@@ -0,0 +1,44 @@
+package examen1p2_hectorflores;
+import java.util.ArrayList;
+
+
+public abstract class Facebook extends SocialClass implements Commentable {
+    private ArrayList <Comment> comentarios;
+    
+    public Facebook(String username) {
+        super(username);
+        this.comentarios = new ArrayList<>();
+    }
+    
+    @Override
+    public boolean addComment(Comment comment) {
+        int postID = comment.getPostId();
+
+        if (postID >= 0 && postID < posts.size()) {
+            comentarios.add(comment);
+            return true;
+        } else {
+            System.out.println("PostID invalido");
+            return false;
+        }
+
+    }
+    
+    public void timeline() {
+        for (int i = 0; i < posts.size(); i++) {
+            System.out.println("POST " + (i + 1));
+            System.out.println(posts.get(i));
+
+        for (Comment comment : comentarios) {
+            if (comment.getPostId() == i) {
+                System.out.println(comment);
+            }
+        }
+    }
+}
+   
+      
+    }
+
+    
+
